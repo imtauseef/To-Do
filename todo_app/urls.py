0e74717651a90ef00e871fb31b5b0a9b34724a69
imtauseef <tauseefkhan532@gmail.com>
2021-08-22 20:49:50 +0500

    yeah todo app is done,

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('items', views.create_item, name='items'),
+    path('deletion/<str:pk>/', views.delete_item, name='deletion'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Todo
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def home(request):
-    return render(request, 'todo_app/home_page.html')
+    data = Todo.objects.all().order_by('-date_published')
+    content = {
+        'data': data,
+    }
+    return render(request, 'todo_app/home_page.html', content)
+
+
+def create_item(request):
+    user_text = request.POST['text-item']
+    entry_date = timezone.now()
+    Todo.objects.create(text=user_text, date_published=entry_date)
+    return HttpResponseRedirect('/')
+
+
+def delete_item(request, pk):
+    item = Todo.objects.get(id=pk)
+    item.delete()
+    return HttpResponseRedirect('/')
